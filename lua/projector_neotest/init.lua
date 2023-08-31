10c6ca02d12bd2c392645e0c4663ff5a6aa0252c
Andrej Kenda <andrej.baca14@gmail.com>
2023-08-31 19:00:15 +0200

    updated to new contract

--- lua/projector_neotest/init.lua
+++ lua/projector_neotest/init.lua
@@ -3,16 +3,16 @@ local NeotestOutput = require("projector_neotest.output")
 local M = {}
 
 ---@class NeotestOutputBuilder: OutputBuilder
----@field private hide_tasks boolean
+---@field private group boolean
 ---@field private include_debug boolean
 M.OutputBuilder = {}
 
----@param opts? { hide_tasks: boolean, include_debug: boolean }
+---@param opts? { group: boolean, include_debug: boolean }
 ---@return NeotestOutputBuilder
 function M.OutputBuilder:new(opts)
   opts = opts or {}
   local o = {
-    hide_tasks = opts.hide_tasks or false,
+    group = opts.group or false,
     include_debug = opts.include_debug or false,
   }
   setmetatable(o, self)
@@ -31,45 +31,49 @@ function M.OutputBuilder:mode_name()
   return "neotest"
 end
 
----@param _ configuraiton_picks
----@return configuraiton_picks # picked configs
-function M.OutputBuilder:preprocess(_)
-  ---@type configuraiton_picks
-  local configs = {}
+---@param configuration task_configuration
+---@return boolean
+function M.OutputBuilder:validate(configuration)
+  if configuration and configuration.neotest_mode then
+    return true
+  end
+  return false
+end
 
-  configs["__neotest_output_builder_task_id_1__"] = {
-    scope = "global",
-    group = "test",
-    name = "Run Current Test",
-    neotest_mode = "run_current",
-  }
-  configs["__neotest_output_builder_task_id_2__"] = {
-    scope = "global",
-    group = "test",
-    name = "Run Current File Tests",
-    neotest_mode = "run_file",
+---@param _ task_configuration[]
+---@return task_configuration[]
+function M.OutputBuilder:preprocess(_)
+  ---@type task_configuration[]
+  local configs = {
+    {
+      name = "Run Current Test",
+      neotest_mode = "run_current",
+    },
+    {
+      name = "Run Current File Tests",
+      neotest_mode = "run_file",
+    },
   }
 
   if self.include_debug then
-    configs["__neotest_output_builder_task_id_3__"] = {
-      scope = "global",
-      group = "test",
+    table.insert(configs, {
       name = "Debug Current Test",
       neotest_mode = "debug_current",
-    }
-    configs["__neotest_output_builder_task_id_4__"] = {
-      scope = "global",
-      group = "test",
+    })
+    table.insert(configs, {
       name = "Debug Current File Tests",
       neotest_mode = "debug_file",
-    }
+    })
   end
 
-  -- hide tasks if specified
-  if self.hide_tasks then
-    for _, cfg in pairs(configs) do
-      cfg.presentation = "menuhidden"
-    end
+  -- group tasks if specified
+  if self.group then
+    return {
+      {
+        name = "Neotest",
+        children = configs,
+      },
+    }
   end
 
   return configs
